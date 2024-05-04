235d3723ed8c3be525820a1f3dd2a30d97e55104
Atharva <atharva.sawant1919@gmail.com>
2024-05-04 23:05:34 +0530

    changes

---
+++ Morelat/more.js
@@ -0,0 +1,47 @@
+var bookmarkButtons = document.querySelectorAll('.bookmark');
+bookmarkButtons.forEach(function (bookmarkButton) {
+    bookmarkButton.addEventListener('click', function (event) {
+        var recipeBox = event.target.closest('.box');
+        if (recipeBox) {
+            var recipeId = recipeBox.id;
+            var recipeName = recipeBox.querySelector('.box-content span').innerText;
+
+            if (bookmarkButton.classList.contains('bookmarked')) {
+                bookmarkButton.classList.remove('bookmarked');
+                removeBookmarkedRecipe(recipeId);
+            } else {
+                bookmarkButton.classList.add('bookmarked');
+                saveBookmarkedRecipe(recipeId, recipeName);
+            }
+        }
+    });
+});
+
+function saveBookmarkedRecipe(recipeId, recipeName) {
+    var bookmarkedRecipes = JSON.parse(localStorage.getItem('bookmarkedRecipes')) || [];
+    bookmarkedRecipes.push({ id: recipeId, name: recipeName });
+    localStorage.setItem('bookmarkedRecipes', JSON.stringify(bookmarkedRecipes));
+    alert('Recipe bookmarked: ' + recipeName);
+}
+
+function removeBookmarkedRecipe(recipeId) {
+    var bookmarkedRecipes = JSON.parse(localStorage.getItem('bookmarkedRecipes')) || [];
+    var updatedRecipes = bookmarkedRecipes.filter(function (recipe) {
+        return recipe.id !== recipeId;
+    });
+    localStorage.setItem('bookmarkedRecipes', JSON.stringify(updatedRecipes));
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var boxes = document.querySelectorAll('.box');
+    var bookmarkedRecipes = JSON.parse(localStorage.getItem('bookmarkedRecipes')) || [];
+    boxes.forEach(function (box) {
+        var recipeId = box.id;
+        var bookmarkButton = box.querySelector('.bookmark');
+        if (bookmarkedRecipes.some(function (recipe) { return recipe.id === recipeId; })) {
+            bookmarkButton.classList.add('bookmarked');
+        }
+    });
+});
+
+
